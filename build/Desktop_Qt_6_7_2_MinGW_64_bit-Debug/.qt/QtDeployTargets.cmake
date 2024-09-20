6f66e70c561ace7ce04a378cdf6383e0026e39bb
zhanganjie <anjing.public@gmail.com>
2024-09-20 09:51:52 +0800

    crm

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_crm_FILE D:/Project/study/crm/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/crm.exe)
+set(__QT_DEPLOY_TARGET_crm_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_crm_RUNTIME_DLLS D:/Software/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Software/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;D:/Software/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Software/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
